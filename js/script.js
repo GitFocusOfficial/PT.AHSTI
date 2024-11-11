318e225c80d9482a85a7075ad68dfaebe6528020
GitFocus <43635097+GitFocusOfficial@users.noreply.github.com>
2024-11-12 02:47:38 +0700

    Added contents

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const captcha = document.getElementById("captcha");
+    const generateCaptcha = () => {
+      const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let captchaText = "";
+      for (let i = 0; i < 6; i++) {
+        captchaText += chars.charAt(Math.floor(Math.random() * chars.length));
+      }
+      captcha.value = captchaText;
+    };
+    generateCaptcha();
+  });
+  
